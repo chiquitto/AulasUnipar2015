4035487b1d0b33041aa69d18428dc2af5168f6f4
Alisson Chiquitto <chiquitto@gmail.com>
2015-03-12 17:44:29 -0300

    3serie - PHP OO - Autoload de classes

--- tads-3serie-servicos-internet/design-patterns-php/mvc/sistema/categoria-cadastrar.php
+++ tads-3serie-servicos-internet/design-patterns-php/mvc/sistema/categoria-cadastrar.php
@@ -1,13 +1,6 @@
 <?php
 
 require 'config.php';
-require './classes/Controller.php';
-require './classes/Controller/Categoria.php';
-require './classes/View.php';
-require './classes/Dao.php';
-require './classes/Vo.php';
-require './classes/Vo/Categoria.php';
-require './classes/Dao/Categoria.php';
 
 $controller = new Controller_Categoria();
 $controller->cadastrarAcao();
