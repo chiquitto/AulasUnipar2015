e64ae93c17e1beccb10bd87bb53c34f41527001b
Alisson Chiquitto <chiquitto@gmail.com>
2015-05-12 18:16:44 -0300

    3serie - Webservice - Validacao de informacoes

--- tads-3serie-servicos-internet/webservices/script/json/categoria-cadastrar.php
+++ tads-3serie-servicos-internet/webservices/script/json/categoria-cadastrar.php
@@ -4,11 +4,22 @@ require '../config.php';
 
 $categoria = $_POST['categoria'];
 
+// Validacoes
 if ($categoria == '') {
     saidaJson(array(), Erros::CATEGORIA_VAZIO);
 }
 
 $con = Conexao::getInstance();
+
+$sql = "Select idcategoria From categoria
+    Where (categoria = '$categoria')";
+$resultado = $con->query($sql);
+$categoriaBd = $resultado->fetch(PDO::FETCH_ASSOC);
+if ($categoriaBd) {
+    saidaJson(array(
+        'idcategoria' => (int) $categoriaBd['idcategoria'],
+    ), Erros::CATEGORIA_EXISTENTE);
+}
 
 $sql = "Insert Into categoria (categoria)
     Values ('$categoria')";
--- tads-3serie-servicos-internet/webservices/script/json/post-cadastrar.php
+++ tads-3serie-servicos-internet/webservices/script/json/post-cadastrar.php
@@ -2,11 +2,34 @@
 
  require '../config.php';
  
- $idcategoria = $_POST['idcategoria'];
+ $idcategoria = (int) $_POST['idcategoria'];
  $titulo = $_POST['titulo'];
  $texto = $_POST['texto'];
-
+ 
  $con = Conexao::getInstance();
+ $sql = "select idcategoria from categoria
+            where idcategoria = $idcategoria";
+ $consulta = $con->query($sql);
+ $idcategoriaBd = $consulta->fetch(PDO::FETCH_ASSOC);
+ if(!$idcategoriaBd){
+     saidaJson(array(), Erros::CATEGORIA_INEXISTENTE);
+ }
+ 
+ $sql = "SELECT IDPOST FROM POST WHERE TITULO ='$titulo'";
+ $result = $con->query($sql);
+ $teste = $result->fetch(PDO::FETCH_ASSOC);
+ if($teste){
+     saidaJson(array(
+         'idpost' => (int) $teste['IDPOST']
+     ), Erros::POST_TITULO_EXISTENTE);
+     exit;
+ }
+     
+ $contarTexto = strlen($texto);
+ if($contarTexto <50){
+     saidaJson(array(),  Erros::POST_TEXTO_MENOR) ;
+ }
+ 
  $sql = "insert into post(idcategoria,titulo,texto) values($idcategoria,'$titulo','$texto')";
  $con->exec($sql);
  $idpost = (int) $con->lastInsertId();
