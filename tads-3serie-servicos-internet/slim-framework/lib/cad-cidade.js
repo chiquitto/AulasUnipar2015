bb54f01014701070bf4d51f7a68f9e5bab075d18
Alisson Chiquitto <chiquitto@gmail.com>
2015-08-23 08:53:01 -0300

    3serie - Ajax & Slim Framework

--- tads-3serie-servicos-internet/slim-framework/lib/cad-cidade.js
+++ tads-3serie-servicos-internet/slim-framework/lib/cad-cidade.js
@@ -1,5 +1,76 @@
 $(document).ready(init);
 
 function init() {
-	window.alert('Oi');
+	//window.alert('Oi');
+	//exibirLoading();
+	carregarUF();
+	$('#frm1').submit(frm1Submit);
 }
+
+function exibirLoading(){
+	$('#loading').show();
+}
+
+function esconderLoading(){
+	$('#loading').hide();
+}
+
+function carregarUF(){
+	exibirLoading();
+	$.ajax({
+		type: 'GET',
+		url: './api/estado',
+		data: {},
+		success: carregarUFOK,
+		dataType: 'json',
+	});
+}
+
+function carregarUFOK(dados){
+	//console.log(dados.dados);
+
+	var template = '<option value="{id}">{texto}</option>';
+	var html = '';
+	var ultimo = dados.dados.length;
+
+	for(var i=0; i<ultimo; i++){
+		var uf = dados.dados[i];
+		html += template.replace('{id}', uf.iduf)
+										.replace('{texto}', uf.uf);
+	}
+	$('#fiduf').html(html);
+	esconderLoading();
+}
+
+function frm1Submit(evento){
+	evento.preventDefault();
+	var iduf = $('#fiduf').val();
+	var cidade = $('#fcidade').val();
+	var populacao = $('#fpopulacao').val();
+
+	$.ajax({
+		type: 'POST',
+		url: './api/cidade',
+		data: {
+			cidade: cidade,
+			populacao: populacao,
+			uf: iduf,
+		},
+		dataType: 'json',
+		success: frm1SubmitOK,
+	});
+}
+
+function frm1SubmitOK(dados){
+	window.alert('OK');
+}
+
+
+
+
+
+
+
+
+
+//
--- tads-3serie-servicos-internet/slim-framework/lib/cidades.js
+++ tads-3serie-servicos-internet/slim-framework/lib/cidades.js
@@ -1,5 +1,92 @@
 $(document).ready(init);
 
 function init() {
-	window.alert('Oi');
+	//window.alert('Oi');
+	carregarUF();
 }
+
+function exibirLoading(){
+	$('#loading').show();
+}
+
+function esconderLoading(){
+	$('#loading').hide();
+}
+
+function carregarUF(){
+	exibirLoading();
+	$.ajax({
+		type:'GET',
+		url: './api/estado',
+		data:{},
+		dataType: 'json',
+		success: carregarUFOK,
+	});
+}
+
+function carregarUFOK(dados){
+	var html = '';
+	var template = '<option value="{id}">{texto}</option>';
+	var ultimo = dados.dados.length;
+
+	html += template.replace('{id}', 0)
+									.replace('{texto}', 'Selecione um estado');
+
+	for(var i = 0; i < ultimo; i++){
+		var estado = dados.dados[i];
+
+		html += template.replace('{id}', estado.iduf)
+										.replace('{texto}', estado.uf);
+	}
+	$('#fiduf').html(html);
+	$('#fiduf').change(carregarCidades);
+	esconderLoading();
+}
+
+function carregarCidades(){
+	var ufSelecionada = $('#fiduf').val();
+	exibirLoading();
+	$.ajax({
+		type: 'GET',
+		url: './api/estado/cidade/'+ ufSelecionada,
+		data: {},
+		dataType: 'json',
+		success: carregarCidadesOK,
+
+	});
+}
+
+function carregarCidadesOK(dados){
+	var html = '';
+	var template = '<option value="{idcidade}">{cidade}</option>';
+	var ultimo = dados.dados.length;
+	html += template.replace('{idcidade}', 0)
+									.replace('{cidade}', 'Selecione uma cidade');
+	for (var i = 0; i < ultimo; i++){
+		var cidade = dados.dados[i];
+		html += template.replace("{idcidade}", cidade.idcidade)
+		.replace("{cidade}", cidade.cidade);
+	}
+	$('#fidcidade').html(html);
+	$('#fidcidade')
+		.unbind('change')
+		.change(carregarPopulacao);
+	esconderLoading();
+}
+
+function carregarPopulacao() {
+	window.alert('Populacao');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//
