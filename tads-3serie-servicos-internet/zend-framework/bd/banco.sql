76494c4673e75fa1bbd52b6e289e65f77f79dd61
Alisson Chiquitto <chiquitto@gmail.com>
2015-10-07 14:26:58 -0300

    3serie - ZF db

---
+++ tads-3serie-servicos-internet/zend-framework/bd/banco.sql
@@ -0,0 +1,27 @@
+CREATE TABLE IF NOT EXISTS `categoria` (
+  `idcategoria` INT NOT NULL AUTO_INCREMENT,
+  `categoria` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`idcategoria`))
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `post` (
+  `idpost` INT NOT NULL AUTO_INCREMENT,
+  `idcategoria` INT NOT NULL,
+  `titulo` VARCHAR(45) NOT NULL,
+  `texto` TEXT NULL,
+  PRIMARY KEY (`idpost`),
+  INDEX `fk_post_categoria_idx` (`idcategoria` ASC),
+  CONSTRAINT `fk_post_categoria`
+    FOREIGN KEY (`idcategoria`)
+    REFERENCES `categoria` (`idcategoria`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+INSERT INTO `categoria` (`idcategoria`, `categoria`) VALUES (NULL, 'Esportes');
+INSERT INTO `categoria` (`idcategoria`, `categoria`) VALUES (NULL, 'Politica');
+INSERT INTO `categoria` (`idcategoria`, `categoria`) VALUES (NULL, 'Games');
+
+INSERT INTO `post` (`idcategoria`, `titulo`, `texto`) VALUES (1, 'Jogo de Futebol', 'Neste ultimo final de semana, os times jogaram ...');
+INSERT INTO `post` (`idcategoria`, `titulo`, `texto`) VALUES (1, 'Mundial de Voley', 'O Brasil venceu a seleção da Servia e Montenegro');
+INSERT INTO `post` (`idcategoria`, `titulo`, `texto`) VALUES (3, 'Lançamento de Call Of Dutty XXI', 'Nesta semana será lançado o novo jogo da série Call Of Dutty');
