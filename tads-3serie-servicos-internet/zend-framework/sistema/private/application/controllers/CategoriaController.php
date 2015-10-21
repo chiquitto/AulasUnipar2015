a70bb67698fccaef2ff6fb3d5a6160ab7c9b4b45
Alisson Chiquitto <chiquitto@gmail.com>
2015-10-21 17:18:15 -0200

    3serie - Editar registros

--- tads-3serie-servicos-internet/zend-framework/sistema/private/application/controllers/CategoriaController.php
+++ tads-3serie-servicos-internet/zend-framework/sistema/private/application/controllers/CategoriaController.php
@@ -35,5 +35,53 @@ class CategoriaController extends Zend_Controller_Action {
 
         $this->view->form = $form;
     }
+    
+    public function editarAction() {
+        $idcategoria = (int) $this->getRequest()->getParam('idcategoria', 0);
+        $categoriatb = new Application_Model_DbTable_Categoria();
+        $categoria = $categoriatb->fetchRow("idcategoria = $idcategoria");
+        
+        if(!$idcategoria) {
+            echo 'ID da Categoria nao eh valido';
+            exit();
+        }
+        
+        $form = new Application_Form_Categoria();
+        
+        if($this->getRequest()->isPost()) {
+            $valores = $this->getRequest()->getParams();
+            
+            if ($form->isValid($valores)) {
+                $valor = $form->getValues();
+                $update = array(
+                    'categoria' => $valores['categoria']
+                );
+                $categoriatb->update($valor, "idcategoria = $idcategoria");
+                $this->_helper->redirector->gotoSimpleAndExit("index");
+            }
+            
+        } else {
+            $form->populate($categoria->toArray());
+        }
+        
+        $this->view->form = $form;
+    }
+    
+    public function excluirAction() {
+        $idcategoria = (int) $this->getRequest()->getParam('idcategoria', 0);
+        $categoriatb = new Application_Model_DbTable_Categoria();
+        
+        $posttb = new Application_Model_DbTable_Post();
+        $post = $posttb->fetchRow("idcategoria = $idcategoria");
+        
+        if (!$post) {
+            $categoriatb->delete("idcategoria = $idcategoria");
+        } else {
+            echo 'Existe Post(s) com essa Categoria';
+            exit();
+        }
+        
+        $this->view->form = $form;
+    }
 
 }
--- tads-3serie-servicos-internet/zend-framework/sistema/private/application/controllers/PostController.php
+++ tads-3serie-servicos-internet/zend-framework/sistema/private/application/controllers/PostController.php
@@ -44,19 +44,40 @@ class PostController extends Zend_Controller_Action {
         $idpost = (int)$this->getRequest()->getParam('idpost', 0);
         $posttb = new Application_Model_DbTable_Post();
         $post = $posttb->fetchRow("idpost = $idpost");
+        
         if (!$post){
             echo "idpost invalido";
             exit;
         }
+        
         $form = new Application_Form_Post();
+        
         if($this->getRequest()->isPost()){
+            $valores = $this->getRequest()->getParams();
+            
+            if ($form->isValid($valores)) {
+                $valor = $form->getValues();
+                $update = array(
+                    'idcategoria' => $valor['idcategoria'], 
+                    'titulo' => $valor['titulo'], 
+                    'texto' => $valor['texto']
+                );
+                $posttb->update($update, "idpost = $idpost");
+                $this->_helper->redirector->gotoSimpleAndExit('index');
+            }
             
         }else{
             $form->populate($post->toArray());
         }
+        
         $this->view->form = $form; 
-        
-        
     }
-
+    
+    public function apagarAction() {
+        $idpost = (int) $this->getRequest()->getParam('idpost');
+        $posttb = new Application_Model_DbTable_Post();
+        $posttb->delete("idpost = $idpost");
+        
+        $this->_helper->redirector->gotoSimpleAndExit('index');
+    }
 }
