85d29f0ebb7f605fe533e08ac489205fcd18c531
Alisson Chiquitto <chiquitto@gmail.com>
2015-10-31 08:53:14 -0200

    3serie - Zend Navigation

--- tads-3serie-servicos-internet/zend-framework/sistema/private/application/Bootstrap.php
+++ tads-3serie-servicos-internet/zend-framework/sistema/private/application/Bootstrap.php
@@ -8,5 +8,17 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         $view = $this->getResource('view');
         $view->doctype('XHTML1_STRICT');
     }
+    
+    protected function _initNavigation() {
+        $this->bootstrap('layout');
+        
+        $xml = APPLICATION_PATH . '/configs/navigation.xml';
+        $config = new Zend_Config_Xml($xml, 'nav');
+        $container = new Zend_Navigation($config);
+        
+        $layout = $this->getResource('layout');
+        $view = $layout->getView();
+        $view->navigation($container);
+    }
 }
 
